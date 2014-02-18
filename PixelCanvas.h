9ae01c56f311893c60b566ee70a0cffe3e7a34e5
Erkki Salonen <erkki.salonen@tpnet.fi>
2014-02-18 08:36:33 +0200

    Doxygen comment updates.

--- PixelCanvas.h
+++ PixelCanvas.h
@@ -19,17 +19,44 @@ private:
 	typedef Gain::Bitmap super;
 
 public:
-	PixelCanvas(int x, int y, int width, int height);
-	PixelCanvas(int width, int height);
-	virtual ~PixelCanvas();
 
-	virtual void updateG(float,float);
+	/**
+	 * Create PixelCanvas to specified location on screen.
+	 *
+	 *
+	 * \param x x-coordinate of canvas start corner.
+	 * \param y y-coordinate of canvas start corner.
+	 * \param width the width of canvas.
+	 * \param height the height of canvas.
+	 */
+
+	PixelCanvas(int x, int y, int width, int height);
+
+	/**
+	 * Create PixelCanvas with certain width and height
+	 **
+	 * \param width the width of canvas.
+	 * \param height the height of canvas.
+	 */
+
+	PixelCanvas(int width, int height);
+
+	virtual ~PixelCanvas();
 
 	void setPixel(int x, int y, char alpha,char blue,char green,char red);
 	void setPixel(int x, int y, unsigned int abgr);
 
 	void getPixel(int x, int y, char *alpha,char *blue,char *green,char *red);
 	void getPixel(int x, int y, unsigned int *abgr);
+
+	/**
+	 * Canvas render function. This is called by Gain framework.
+	 **
+	 * \param sec elapsed time since start.
+	 * \param deltaSec elapsed time last update.
+	 */
+
+	virtual void updateG(float sec,float deltaSec);
 protected:
 
 	virtual bool initVariables();
