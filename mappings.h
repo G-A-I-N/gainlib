312113e3f0e1345ef3d08e695ee66173f550a9f8
Ville Kankainen <ville.kankainen@gmail.com>
2014-03-20 22:27:53 +0200

    updated licenses and dos2unix
    Change-Id: I9e135f49285e335c748551489d010d736dd56dbd

--- mappings.h
+++ mappings.h
@@ -1,9 +1,20 @@
-/*
- * mappings.h
- *
- *  Created on: Feb 2, 2013
- *      Author: villekankainen
- */
+// Copyright 2014
+//   erkki.salonen@tpnet.fi
+//   not.committed.yet
+//   ville.kankainen@gmail.com
+//   ville.kankainen@kangain.com
+// 
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an AS IS BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef MAPPINGS_H_
 #define MAPPINGS_H_
@@ -13,12 +24,11 @@
 #include <stdint.h>
 #include <string>
 
+#ifdef LOGISON
+#define logmessage  "now using logging"
 
+#else
 #define STORE_RELEASE
-
-// Undefine following if wanting disable logging
-#if 0
-#define LOGISON
 #endif
 
 
